6d1acd899e586e07c5d026a54ecc38ef2001b714
AnthonyRyu <ryuhuigwon@gmail.com>
2023-04-18 14:45:58 +0900

    initial commit

--- quasar-vue/src/router/routes.js
+++ quasar-vue/src/router/routes.js
@@ -6,6 +6,7 @@ const routes = [
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') },
       { path: '/vue', component: () => import('pages/VueEx.vue') },
+      { path: '/todo', component: () => import('pages/Todo.vue') },
     ]
   },
 
